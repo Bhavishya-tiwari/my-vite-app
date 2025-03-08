b4f9ef404d9e605a1769fbdd472d5b49aaaa2a7c
cb-bhavishya-tiwari <bhavishya.tiwari@chargebee.com>
2025-03-08 16:15:07 +0530

    tailwind addes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ function App() {
 
   return (
     <>
-      <p>bihhihihih</p>
+        <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/my-vite-app/', // This is for setting the base path when deploying to a subdirectory.
 });
