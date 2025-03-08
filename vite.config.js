d7edd3f10ea9e9afc16647b0d181bfc4a7a8f105
cb-bhavishya-tiwari <bhavishya.tiwari@chargebee.com>
2025-03-08 16:09:41 +0530

    zz

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-vite-app/', // Set this to your repsitoy name
+  plugins: [react()],
+  base: '/my-vite-app/', // This is for setting the base path when deploying to a subdirectory.
 });
