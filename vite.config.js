a80f3df9fbcbd669bb3de116acb03ec02a540d7a
cb-bhavishya-tiwari <bhavishya.tiwari@chargebee.com>
2025-03-08 15:46:50 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/my-vite-app/', // Set this to your repository name
+  base: '/my-vite-app/', // Set this to your repositoy name
 });
