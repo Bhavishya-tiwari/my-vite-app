2edb42cc5913eaae57f3829c0b79187522a7acc9
cb-bhavishya-tiwari <bhavishya.tiwari@chargebee.com>
2025-03-10 19:38:39 +0530

    updated home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,13 @@ function About() {
       <p>Learn more about us.</p>
 
       {/* Iframe embedding the external zPricing Page */}
-      <iframe
+      {/* <iframe
         src="https://hosted.atomicpricing.com/sites/01JN5WKGC6GSN9542J6KS3TS7G/pricing/01JN5WKHRJPY5EC0EY7GYPVK01?gtmId=GTM-THKXWQPN"
         width="100%"
         height="600px"
         style={{ border: 'none' }}
         allowFullScreen
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 }
